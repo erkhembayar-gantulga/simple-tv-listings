d9d19ecdadce91ce3d50fe685631e6df8bb0e941
Erkhembayar Gantulga <erheme318@gmail.com>
2015-12-06 01:21:55 +0100

    Added admin CR(UD) functionality
     - Added create and read functionality of Channel and Listing
       entities

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 use TVListings\Domain\Service\DoctrineEntityManager;
 use TVListings\Domain\Repository\ChannelRepository;
+use TVListings\Domain\Repository\ListingRepository;
+use TVListings\Domain\Entity\Channel;
+use TVListings\Domain\Entity\Listing;
 
 if (PHP_SAPI == 'cli-server') {
     // To help the built-in PHP dev server, check if the request was actually for
@@ -23,6 +26,7 @@ $configuration = array(
     'settings' => array(
         'displayErrorDetails' => true,
         'doctrine' => array(
+            'mapping_path' => __DIR__ . '/../src/Domain/Entity',
             'driver' => 'pdo_mysql',
             'dbname' => 'mn_tv_listings',
             'user' => 'root',
@@ -91,6 +95,11 @@ $container['tvlistings.channel.repository'] = function ($container) {
 
     return new ChannelRepository($em);
 };
+$container['tvlistings.listing.repository'] = function ($container) {
+    $em = new DoctrineEntityManager($container->get('doctrine.orm.entity_manager'));
+
+    return new ListingRepository($em);
+};
 
 $app->get('/', function ($request, $response, $args) {
     $container = $this->getContainer();
@@ -108,6 +117,112 @@ $app->get('/', function ($request, $response, $args) {
     );
 
     return $response->write($body);
+});
+
+$app->get('/admin', function ($request, $response, $args) {
+    $container = $this->getContainer();
+
+    $channels = $container->get('tvlistings.channel.repository')->findAll();
+
+    $this->view->render(
+        $response,
+        'admin/list.html.twig',
+        array(
+            'channels' => $channels,
+        )
+    );
+
+    return $response->write($body);
+})->setName('admin_homepage');
+
+$app->map(['GET', 'POST'], '/admin/channels/new', function ($request, $response, $args) {
+
+    if ($request->isPost()) {
+        $container = $this->getContainer();
+        $parsedBody = $request->getParsedBody();
+        $channelRepository = $container->get('tvlistings.channel.repository');
+        $channel = new Channel($parsedBody['name'], $parsedBody['logoPath']);
+        $channelRepository->persist($channel);
+    }
+    $this->view->render(
+        $response,
+        'admin/new.html.twig',
+        array()
+    );
+
+    return $response->write($body);
+})->setName('admin_channel_new');
+
+$app->group('/admin/channels/{slug}', function () {
+    $this->delete('', function ($request, $response, $args) {
+        if ($request->isDelete()) {
+            echo "blah";die;
+        }
+        $container = $this->getContainer();
+        $parsedBody = $request->getParsedBody();
+        $channel = $container->get('tvlistings.channel.repository')->findOneBySlug($slug);
+        $channelRepository->remove($channel);
+
+        $this->view->render(
+            $response,
+            'admin/new.html.twig',
+            array()
+        );
+
+        return $response->write($body);
+    })->setName('admin_channel_delete');
+
+    $this->get('', function ($request, $response, $args) {
+        $container = $this->getContainer();
+        $parsedBody = $request->getParsedBody();
+        $channel = $container->get('tvlistings.channel.repository')->findOneBySlug($args['slug']);
+
+        $listings = $this->getContainer()->get('tvlistings.listing.repository')
+            ->findBy($channel);
+
+        $this->view->render(
+            $response,
+            'admin/show.html.twig',
+            array(
+                'channel' => $channel,
+                'listings' => $listings,
+            )
+        );
+
+        return $response->write($body);
+    })->setName('admin_channel_show');
+
+    $this->map(['GET', 'POST'], '/listings/new', function ($request, $response, $args) {
+
+        $channel = $this->getContainer()->get('tvlistings.channel.repository')->findOneBySlug($args['slug']);
+        if ($request->isPost()) {
+            $parsedBody = $request->getParsedBody();
+            $listingRepository = $this->getContainer()->get('tvlistings.listing.repository');
+            $listing = new Listing($channel, $parsedBody['title'], new \DateTime($parsedBody['programDate']), $parsedBody['resourceLink']);
+            $listing->programAt($parsedBody['programAt']);
+            $listing->setDescription($parsedBody['description']);
+            $listingRepository->persist($listing);
+
+            $uri = $this->router->pathFor(
+                'admin_channel_show',
+                array(
+                    'slug' => $channel->getSlug(),
+                )
+            );
+
+            return $response->withRedirect((string)$uri, 301);
+        }
+
+        $this->view->render(
+            $response,
+            'admin/Listing/new.html.twig',
+            array(
+                'channel' => $channel,
+            )
+        );
+
+        return $response->write($body);
+    })->setName('admin_listing_new');
 });
 
 // Run!
